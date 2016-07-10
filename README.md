53834c0bec5d593a5ae0e60f58274cbcc3f66a9b
Tushar Pokle <tushar.pokle@gmail.com>
2016-07-10 17:37:16 +1000

    webpack and its cohorts are in the project now

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 
+# Project layout
 
 Following the instructions from
 
 https://www.typescriptlang.org/docs/handbook/react-&-webpack.html
 
+# Build
+
+    npm i
+    npm link typescript
+    typings install
+    webpack
+
+# Dev
+
+    npm i -g webpack-dev-server
+    webpack-dev-server
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,9 +1,26 @@
 import * as React from "react";
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface HelloProps { compiler: string, framework: string }
+export interface HelloState { count: number }
 
-export class Hello extends React.Component<HelloProps, {}> {
+export class Hello extends React.Component<HelloProps, HelloState> {
+
+    constructor(props: HelloProps) {
+        super(props)
+        this.state = { count: 1 }
+        setInterval(this.tick, 1000)
+    }
+
+    tick = () => {
+        this.setState({ count: this.state.count + 1 })
+    }
+
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return (
+            <div>
+                <h1>And now Hello from {this.props.compiler} and {this.props.framework}!</h1>
+                You've starteded at this page for {this.state.count} seconds.
+            </div>
+        );
     }
 }
