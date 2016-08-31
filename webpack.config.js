d67ff1abc137e9ec067a1b5fee9f222f2c1a45e6
Tushar Pokle <tushar.pokle@gmail.com>
2016-08-31 21:32:14 +1000

    index + app

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,26 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     context: __dirname,
     entry: {
-        index: "./src/index.tsx"
+        index: "./src/index.tsx",
+        app: "./src/app.tsx"
     },
     output: {
         path: "public/dist",
         filename: "[name].js",
-        publicPath: "/dist/"
+        publicPath: "/dist/",
+        sourceMapFilename: '[name].js.map'
     },
 
-    // Enable sourcemaps for debugging webpack's output.
     devtool: "cheap-module-source-map",
 
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
 
     module: {
         loaders: [
             { test: /\.tsx?$/, loader: "ts-loader" },
-            { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" }
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader") }
         ],
 
         preLoaders: [
@@ -32,6 +32,8 @@ module.exports = {
     },
 
     plugins: [
+        new ExtractTextPlugin("[name].css"),
+
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify(process.env.NODE_ENV),
